7794adb0273eea12da7698ec9e8415fc59cc2905
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-09 16:32:10 +0530

    Calculating the Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,9 +5,20 @@ echo "------Welcome to EmployeeWageComputation-----"
 
 randomcheck=$((RANDOM%2))
 
+
+IsFullTime=8
+EmpRate=20
+
+# Random Chec Employee is Present or Absent
+
 if [ $randomcheck == 0 ]
 then
 	echo "Employees is Absent"
 else
 	echo  "Employee is Present"
 fi
+
+# Calulating the Employee Wage
+
+DailyWage=$(($IsFullTime*$EmpRate))
+echo "Employee Daily Wage is:$DailyWage"
