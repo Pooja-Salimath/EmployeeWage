9c499da24d8cea000b35d86082983a840876c32d
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-09 16:12:44 +0530

    Employee Present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,3 +3,11 @@
 
 echo "------Welcome to EmployeeWageComputation-----" 
 
+randomcheck=$((RANDOM%2))
+
+if [ $randomcheck == 0 ]
+then
+	echo "Employees is Absent"
+else
+	echo  "Employee is Present"
+fi
