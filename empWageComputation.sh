12f93527c3e60a2f7cc5ee7106196d6101177cd8
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-09 19:11:02 +0530

    Calculate Wage by Assume 100 hrs and 20 days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -22,19 +22,19 @@ else
 	# Calculating the Employee Wage
 
 	DailyWage=$(($IsFullTime*$EmpRate))
-	echo "Employee Daily Wage is:$DailyWage"
+	echo "Daily Wage for Full Time is:$DailyWage"
 
 	# Calculating the Employee Wage for PartTime Employee
 
 	PartTimeWage=$(($isPartTime*$EmpRate))
-	echo "Employee Daily Wage for part time employee is: $PartTimeWage"
+	echo "Daily Wage for Part Time Employee is: $PartTimeWage"
 
 
 	# Solving using Switch Case Statement
 
-	#empcheck=$((RANDOM%3))
+	empcheck=$((RANDOM%3))
 
-	case $randomcheck in
+	case $empcheck in
 		1)
 			empHrs=$IsFullTime
 			;;
@@ -54,4 +54,28 @@ else
 	WagePerMonth=$(($Wage*$RatePerMonth))
 	echo "Monthly Wage for Employee:$WagePerMonth"
 
+	# Calculate Wages till a condition of total	working hours or	days is reached for	a month - Assume 100 hours
+
+	TotalWorkHours=0
+	WorkingDays=0
+	TotalMonthwage=0
+	while [[ $TotalWorkHours -le 100 || $WorkingDays -le 20 ]]
+	do
+		 case $empcheck in
+      1)
+         empHrs=$IsFullTime
+         ;;
+      2)
+         empHrs=$isPartTime
+         ;;
+      *)empHrs=0
+         ;;
+    esac
+
+     Wage=$((empHrs*$EmpRate))
+	  WagePerMonth=$(($Wage*$RatePerMonth))
+	  TotalWorkHours=$((TotalWorkHours+empHrs))
+	  WorkingDays=$((WorkingDays+1))
+	  done
+	  echo "Monthly Wage is:$WagePerMonth"
 fi
