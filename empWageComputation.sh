77ebd3559062ac93c9f8339a131aedfdf14ed2be
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-09 18:40:13 +0530

    Calculating Wages Per Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,6 +9,7 @@ randomcheck=$((RANDOM%2))
 IsFullTime=8
 isPartTime=4
 EmpRate=20
+RatePerMonth=20
 
 # Random Chec Employee is Present or Absent
 
@@ -17,35 +18,40 @@ then
 	echo "Employees is Absent"
 else
 	echo  "Employee is Present"
+
+	# Calculating the Employee Wage
+
+	DailyWage=$(($IsFullTime*$EmpRate))
+	echo "Employee Daily Wage is:$DailyWage"
+
+	# Calculating the Employee Wage for PartTime Employee
+
+	PartTimeWage=$(($isPartTime*$EmpRate))
+	echo "Employee Daily Wage for part time employee is: $PartTimeWage"
+
+
+	# Solving using Switch Case Statement
+
+	#empcheck=$((RANDOM%3))
+
+	case $randomcheck in
+		1)
+			empHrs=$IsFullTime
+			;;
+		2)
+			empHrs=$isPartTime
+			;;
+		*)empHrs=0
+			;;
+	esac
+
+	Wage=$((empHrs*$EmpRate))
+	echo "Total Wage is: $Wage"
+
+	# Calculating Wages for a Month
+
+
+	WagePerMonth=$(($Wage*$RatePerMonth))
+	echo "Monthly Wage for Employee:$WagePerMonth"
+
 fi
-
-# Calculating the Employee Wage
-
-DailyWage=$(($IsFullTime*$EmpRate))
-echo "Employee Daily Wage is:$DailyWage"
-
-# Calculating the Employee Wage for PartTime Employee
-
-PartTimeWage=$(($isPartTime*$EmpRate))
-echo "Employee Daily Wage for part time employee is: $PartTimeWage"
-
-
-# Solving using Switch Case Statement
-
-empcheck=$((RANDOM%3))
-
-case $empcheck in
-	1)
-		empHrs=$IsFullTime
-		;;
-	2)
-		empHrs=$isPartTime
-		;;
-	*)
-		empHrs=0
-		;;
-esac
-
-Wage=$((empHrs*$EmpRate))
-echo "Total Wage is: $Wage"
-
